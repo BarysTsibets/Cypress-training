72e2c42387045c76bd43fd5811b55b4e2b725c52
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-06 18:42:19 -0700

    type in the search field

--- Locators.spec.js
+++ Locators.spec.js
@@ -4,9 +4,13 @@ describe('End to end test', function(){
 
     it('Add to cart full flow', function(){
       
-        
+        cy.visit('https://demo.nopcommerce.com/')
+
+        cy.get('#small-searchterms').type("Apple MacBook Pro 13-inch") // type 'Apple MacBook Pro 13-inch' in search field
+        cy.get('#small-search-box-form > .button-1').click()           // click on the search button
 
 
-        
+
+
     })
 })
