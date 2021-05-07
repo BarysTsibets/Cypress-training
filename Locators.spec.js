1d031080bd44ed78549fdbd040b0791d36950124
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-06 18:53:29 -0700

    go to the cart action added

--- Locators.spec.js
+++ Locators.spec.js
@@ -8,7 +8,10 @@ describe('End to end test', function(){
 
         cy.get('#small-searchterms').type("Apple MacBook Pro 13-inch") // type 'Apple MacBook Pro 13-inch' in search field
         cy.get('#small-search-box-form > .button-1').click()           // click on the search button
-
+        cy.get('.product-box-add-to-cart-button').click()              // add to the cart
+        cy.get('#product_enteredQuantity_4').clear().type('2')          //clear and type qyantity 2 
+        cy.get('#add-to-cart-button-4').click()                         // click add to the cart button
+        cy.get('.cart-label').click()                                   // go to the shopping cart
 
 
 
