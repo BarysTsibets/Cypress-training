590bfcee070e2520a9e7e0fdbdedae3f344cc0e9
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-10 16:34:09 -0700

    value presented anywhere added

--- cypress/integration/examples/Table.spec.js
+++ cypress/integration/examples/Table.spec.js
@@ -4,7 +4,11 @@ describe('table actions Suit', function(){
 
     it('Table test', function()
     {
+        cy.visit('http://testautomationpractice.blogspot.com/')
+        
 
+        // 1) Check that Value presented anywhere in the table
+        cy.get('[name="BookTable"]').contains('td', 'Learn Selenium').should('be.visible')
 
 
     })
