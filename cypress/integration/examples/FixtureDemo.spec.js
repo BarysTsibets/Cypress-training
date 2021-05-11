7b0bd3f31e3c82da14af1120da901ba87365f246
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-11 15:00:16 -0700

    admin login flow added

--- cypress/integration/examples/FixtureDemo.spec.js
+++ cypress/integration/examples/FixtureDemo.spec.js
@@ -4,7 +4,11 @@ describe('Fixtures Suit', function(){
 
     it('Fixtures Demo test', function()
     {
+        cy.visit('https://admin-demo.nopcommerce.com/login')
 
-
+        cy.get('#Email').clear().type('admin@yourstore.com')
+        cy.get('#Password').clear().should('be.visible').should('be.enabled').type('admin')
+        cy.get('.button-1').should('be.visible').click()
+        cy.title().should('eq', 'Dashboard / nopCommerce administration')
     })
 })
