658be89b1dcd0985e694b8f81b2c11843ee94cdc
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-10 18:01:44 -0700

    before/after (Each) methods added

--- cypress/integration/examples/HooksDemo.spec.js
+++ cypress/integration/examples/HooksDemo.spec.js
@@ -2,8 +2,39 @@
 
 describe('Hooks suit', function(){
 
-    it('Hooks tests', function()
-    {
+    before(() => {
+       // runs once before all tests in the block
+        cy.log('------ This is Before method ------')
+      })
+    
+      beforeEach(() => {
+        // runs before each test in the block
+        cy.log('------ This is beforeEach method ------')
+      })
+    
+      afterEach(() => {
+        // runs after each test in the block
+        cy.log('------ This is afterEach method ------')
+      })
+    
+      after(() => {
+        // runs once after all tests in the block
+        cy.log('------ This is After method ------')
+      })
 
+
+    it('Search block tests', function()
+    {
+         cy.log('******* This is Search test ******')   
+    })
+
+    it('Advanced search block tests', function()
+    {
+         cy.log('******* This is Advanced Search test ******')   
+    })
+
+    it('Listing Prods block tests', function()
+    {
+         cy.log('******* This is Listing Prods block test ******')   
     })
 })
