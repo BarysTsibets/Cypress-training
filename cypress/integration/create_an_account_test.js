3a763074bc82cbe68af586a8fb4551a2a783c842
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-19 16:22:19 -0700

    refactoring using POM

--- cypress/integration/create_an_account_test.js
+++ cypress/integration/create_an_account_test.js
@@ -1,23 +1,45 @@
 /// <reference types = "cypress"/>
 
+import CreateAnAccount from "../pages/create_an_account_page"
+
+
 describe ('Create account test suit', function(){
 
-    it('Create An Account Smoke test (Positive)', function(){
-        cy.visit('http://automationpractice.com/index.php?controller=authentication&back=my-account')
-        cy.get('#email_create').clear().type('barystsibets556@gmail.com')               //input email to start create account
-        cy.get('#SubmitCreate > span').click()
+    it('Create An Account Smoke test', function(){
 
-        cy.get('.page-heading').should('contain', 'Create an account')                  // 'CREATE AN ACCOUNT' text presented
-        cy.get('#id_gender1').should('be.visible').should('not.be.checked').click()     // Click radiobutton 'Mr' 
-        cy.get('#customer_firstname').clear().type('Bob')                               // input customer 'First Name"
-        cy.get('#customer_lastname').clear().type('Marley')                             // input customer 'Last Name"
-        //add verify emailplaceholder
+        const ca =new CreateAnAccount()
+        
+        ca.visitSignInPage('http://automationpractice.com/index.php?controller=authentication&back=my-account')
+        //cy.visit('http://automationpractice.com/index.php?controller=authentication&back=my-account')
+       
+        ca.enterEmailToCreateAccount('barystsibets557@gmail.com')
+        //cy.get('#email_create').clear().type('barystsibets556@gmail.com')              //input email to start create account
+        
+        ca.clickCreateAnAccountBtn()
+        //cy.get('#SubmitCreate > span').click()
 
-        cy.get('#passwd').clear().type('123456')                                        //input password
+        ca.CreateAnAccountTextPresented().should('contain', 'Create an account')          // 'CREATE AN ACCOUNT' text presented
+        //cy.get('.page-heading').should('contain', 'Create an account')   
+                       
+        ca.MrRadiobutton().should('be.visible').should('not.be.checked').click()
+        //cy.get('#id_gender1').should('be.visible').should('not.be.checked').click()     // Click radiobutton 'Mr' 
+        
+        ca.fillFirstName('Bob')
+        //cy.get('#customer_firstname').clear().type('Bob')                               // input customer 'First Name"
+        
+        ca.fillLastName('Marley')
+        //cy.get('#customer_lastname').clear().type('Marley')                             // input customer 'Last Name"
+        
 
-        cy.get('#days').select('27').should('have.value', '27')    
-        cy.get('#months').select('2')
-        cy.get('#years').select('1995')
+        ca.fillPassword('123456')
+        //cy.get('#passwd').clear().type('123456')                                        //input password
+
+        ca.selectDay('27').should('have.value', '27') 
+        //cy.get('#days').select('27').should('have.value', '27')  
+        ca.selectmonth('3')  
+        //cy.get('#months').select('2')
+        ca.selectYear('2000') 
+        //cy.get('#years').select('1995')
 
         cy.get('#newsletter').check().should('be.checked').and('have.value', '1')      //newsletter checkbox
         cy.get('#optin').check().should('be.checked').and('have.value', '1')           //special offer check box
@@ -39,10 +61,9 @@ describe ('Create account test suit', function(){
 
         cy.get('#alias').should('have.value', 'My address').clear().type('test text')
 
-        cy.get('#submitAccount > span').should('be.visible').click()    //click Register button
+        cy.get('#submitAccount > span').should('be.visible').click()                   //click Register button
 
-        cy.title().should('eq', 'My account - My Store')   // 
-
+        cy.title().should('eq', 'My account - My Store')                               // success assertion
 
     })
 })
