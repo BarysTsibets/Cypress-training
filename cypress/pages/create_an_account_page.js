fb02d090aaf7a04fcb52d39b0ec0393a51646ca2
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-20 20:33:06 -0700

    refactoring

--- cypress/pages/create_an_account_page.js
+++ cypress/pages/create_an_account_page.js
@@ -12,6 +12,20 @@ PasswordField = '#passwd'
 DayDropDown = '#days'
 monthDropDown = '#months'
 yearDropDown = '#years'
+newsletterCheckBox = '#newsletter'
+specialOfferCheckBox = '#optin'
+CompanyField = '#company'
+Adress1Field = '#address1'
+Adress2Field = '#address2'
+CompanyCityField = '#city'
+stateDropDown = '#id_state'
+postCodeField = '#postcode'
+Country = '#id_country'
+additionalInfoField = '#other'
+homePhoneNumber = '#phone'
+mobilePhoneNumber = '#phone_mobile'
+aliasAddress = '#alias'
+registerBtn = '#submitAccount > span'
 
     
 
@@ -59,8 +73,61 @@ selectYear(year){
     return cy.get(this.yearDropDown).select(year)
 }
 
-
+ChecknewsletterCheckBox(){
+    return cy.get(this.newsletterCheckBox).check()
+}
+CheckSpecialOfferCheckbox(){
+    return cy.get(this.specialOfferCheckBox).check()
 }
 
+fillCompanyName(company_name){
+    return cy.get(this.CompanyField).clear().type(company_name)
+}
+
+fillCompanyAddress1(adress1){
+    return cy.get(this.Adress1Field).clear().type(adress1)
+}
+
+fillCompanyAddress2(suit){
+    return cy.get(this.Adress2Field).clear().type(suit)
+}
+
+fillCompanyCity(city){
+    return cy.get(this.CompanyCityField).clear().type(city)
+}
+
+selectState(){
+    return cy.get(this.stateDropDown).select('2')
+}
+
+fillPostCode(zip){
+    return cy.get(this.postCodeField).clear().type(zip)
+}
+
+checkPreselectedCountry(){
+    return cy.get(this.Country)
+}
+
+fillAdditionalInfo(message){
+    return cy.get(this.additionalInfoField).clear().type(message)
+}
+
+fillHomePhone(home_number){
+    return cy.get(this.homePhoneNumber).clear().type(home_number)
+}
+
+fillMobilePhone(mobile_number){
+    return cy.get(this.mobilePhoneNumber).clear().type(mobile_number)
+}
+
+fillAliasAddress(){
+    return  cy.get(this.aliasAddress)
+}
+
+RegisterButton(){
+    return cy.get(this.registerBtn)
+}
+
+}
 
 export default CreateAnAccount
