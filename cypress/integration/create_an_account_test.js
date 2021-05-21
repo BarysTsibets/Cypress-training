27c5ca1f1034c809b05ef8fba9305ae889daec74
Barys Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2021-05-20 20:32:33 -0700

    CA methods added

--- cypress/integration/create_an_account_test.js
+++ cypress/integration/create_an_account_test.js
@@ -1,19 +1,21 @@
 /// <reference types = "cypress"/>
-
 import CreateAnAccount from "../pages/create_an_account_page"
+import faker from 'faker'
 
 
 describe ('Create account test suit', function(){
 
-    it('Create An Account Smoke test', function(){
-
+        it('Create An Account Smoke test', function(){
+            
+        //const my_faker = faker.internet.email()
         const ca =new CreateAnAccount()
         
         ca.visitSignInPage('http://automationpractice.com/index.php?controller=authentication&back=my-account')
         //cy.visit('http://automationpractice.com/index.php?controller=authentication&back=my-account')
-       
-        ca.enterEmailToCreateAccount('barystsibets557@gmail.com')
-        //cy.get('#email_create').clear().type('barystsibets556@gmail.com')              //input email to start create account
+     
+        
+        ca.enterEmailToCreateAccount('fakeemail123@gmail.com')
+        //cy.get('#email_create').clear().type('fakeemail123@gmail.com')              //input email to start create account
         
         ca.clickCreateAnAccountBtn()
         //cy.get('#SubmitCreate > span').click()
@@ -41,29 +43,50 @@ describe ('Create account test suit', function(){
         ca.selectYear('2000') 
         //cy.get('#years').select('1995')
 
-        cy.get('#newsletter').check().should('be.checked').and('have.value', '1')      //newsletter checkbox
-        cy.get('#optin').check().should('be.checked').and('have.value', '1')           //special offer check box
+        ca.ChecknewsletterCheckBox().should('be.checked').and('have.value', '1')
+        //cy.get('#newsletter').check().should('be.checked').and('have.value', '1')      //newsletter checkbox
+        ca.CheckSpecialOfferCheckbox().should('be.checked').and('have.value', '1')
+        //cy.get('#optin').check().should('be.checked').and('have.value', '1')           //special offer check box
+
+        ca.fillCompanyName('ABC company')
+        //cy.get('#company').clear().type('ABC company')                                 // input Company name
+        
+        ca.fillCompanyAddress1('100 hollywood blvd, 90028')
+        //cy.get('#address1').clear().type('100 hollywood blvd, 90028')                  //inpyt Company adress
+        
+        ca.fillCompanyAddress2('suit 500')
+        //cy.get('#address2').clear().type('suit 500')                                   //inpyt Company suit
+
+        ca.fillCompanyCity('Los Angeles')
+        //cy.get('#city').clear().type('Los Angeles')                                    //inpyt Company City
+        
+        ca.selectState('2').should('contain.value', '2') 
+        //cy.get('#id_state').select('2').should('contain.value', '2')                   // Select State 'Alaska'
+        
+        ca.fillPostCode('90028')
+        //cy.get('#postcode').clear().type('90028')                                      //inpyt Zip Code
+
+        ca.checkPreselectedCountry().should('have.value', '21')
+        //cy.get('#id_country').should('have.value', '21')                               // Check that Country preselected as United States
+
+        ca.fillAdditionalInfo('Hello world')
+        //cy.get('#other').clear().type('Hello World')                                   //type additional information
+        
+        ca.fillHomePhone('323 12345678')
+        //cy.get('#phone').clear().type('323 12345678')                                  //type Home Phone
+        
+        ca.fillMobilePhone('323 12345678')
+        //cy.get('#phone_mobile').clear().type('323 12345678')                           //type Mobile phone
 
 
-        cy.get('#company').clear().type('ABC company')                                 // input Company name
-        cy.get('#address1').clear().type('100 hollywood blvd, 90028')                  //inpyt Company adress
-        cy.get('#address2').clear().type('suit 500')                                   //inpyt Company suit
+        ca.fillAliasAddress().should('have.value', 'My address').clear().type('test text')
+        //cy.get('#alias').should('have.value', 'My address').clear().type('test text')
 
-        cy.get('#city').clear().type('Los Angeles')                                    //inpyt Company City
-        cy.get('#id_state').select('2').should('contain.value', '2')                   // Select State 'Alaska'
-        cy.get('#postcode').clear().type('90028')                                      //inpyt Zip Code
+        cy.get(ca.registerBtn).should('be.visible').click()
+        //cy.get('#submitAccount > span').should('be.visible').click()                   //click Register button
 
-        cy.get('#id_country').should('have.value', '21')                               // Check that Country preselected as United States
 
-        cy.get('#other').clear().type('Hello World')                                   //type additional information
-        cy.get('#phone').clear().type('323 12345678')                                  //type Home Phone
-        cy.get('#phone_mobile').clear().type('323 12345678')                           //type Mobile phone
-
-        cy.get('#alias').should('have.value', 'My address').clear().type('test text')
-
-        cy.get('#submitAccount > span').should('be.visible').click()                   //click Register button
-
-        cy.title().should('eq', 'My account - My Store')                               // success assertion
+        cy.title().should('eq', 'My account - My Store')                               // Account Created Page verification
 
     })
 })
